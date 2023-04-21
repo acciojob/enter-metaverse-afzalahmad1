ac0d58a604511007da930377f8a13f0cf179ef06
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-04-22 01:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+let text = document.getElementById("status");
+btn.addEventListener("click",()=>{
+	text.innerHTML = `<h1>Entered Metaverse</h1>`
+})
