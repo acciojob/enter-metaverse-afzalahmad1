a36d5bc13676ff65db6d8d57edd96c8b314eb136
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-04-22 01:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 let btn = document.getElementById("enterBtn");
 let text = document.getElementById("status");
 btn.addEventListener("click",()=>{
-	text.innerHTML = `<h1>Entered Metaverse</h1>`
+	//text.innerHTML = `<h1>Entered Metaverse</h1>`
+	let heading = document.createElement("h1");
+	heading.innerText = "Entered Metaverse"
+	text.innerText = null;
+	text.appendChild(heading);
+	
 })
